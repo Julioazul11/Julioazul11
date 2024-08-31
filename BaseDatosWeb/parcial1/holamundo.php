39677ceb4a8b0ddd430196ecbbf31da6384b46ec
Julioazul11 <159394674+Julioazul11@users.noreply.github.com>
2024-08-30 18:55:16 -0700

    holamundo 30/08/24

---
+++ BaseDatosWeb/parcial1/holamundo.php
@@ -0,0 +1,18 @@
+<?php
+
+   
+$nombre = "luis";
+$numero = 4;
+$numero2 = "4";
+
+   $total = $numero + $numero2;
+
+echo"total = " . $total;
+
+  echo "<h1>Hola Mundo</h1>";
+
+ for($i=0; $i<10; $i++){
+     echo $i . "<br>";
+ }
+   echo "<h3 style='color: red;'> juliolopez</h3>";  
+  ?>
